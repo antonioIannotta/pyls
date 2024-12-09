fd8e84c4958c7ffec4f29940dd78a6ddbb1681b5
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2024-12-09 22:32:24 +0100

    feat: utility function for -h option realized

--- utils.py
+++ utils.py
@@ -16,3 +16,19 @@ def format_time(timestamp):
     :return: the timestamp formatted
     """
     return time.strftime("%Y-%m-%d %H:%M", time.localtime(timestamp))
+
+
+def format_bytes(size: int) -> str:
+    """
+    This function returns the size formatted in human-readable format
+    :param size: the size to be formatted
+    :return: the size formatted as a string
+    """
+    units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB']
+    unit_index = 0
+
+    # Convert to the appropriate unit (greater than 1023 bytes)
+    while size >= 1024 and unit_index < len(units) - 1:
+        size /= 1024
+        unit_index += 1
+    return f"{size:.2f} {units[unit_index]}"
